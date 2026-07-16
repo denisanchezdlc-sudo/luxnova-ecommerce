c22781da003ec20c9c8ab170f030548d6990d85d
denis <denisanchezdlc@gmail.com>
2026-07-16 18:29:35 -0500

    centro de pruebas

--- app.js
+++ app.js
@@ -297,7 +297,7 @@ app.post('/api/tumipay/generar-payin', async (req, res) => {
 
         // Limpiamos cualquier slash extra al final y agregamos la ruta /api estándar
         const baseUrlLimpia = process.env.TUMI_BASE_URL.replace(/\/+$/, '');
-        const response = await fetch(`${baseUrlLimpia}/api/payin`, {
+        const response = await fetch(`https://api-empresas.staging.topup.com.co/production/api/v2/payin`, {
             method: 'POST',
             headers: {
                 'Token-Top': process.env.TUMI_TOKEN_TOP, 
@@ -373,7 +373,7 @@ app.post('/api/tumipay/generar-payout', async (req, res) => {
 
         // Limpiamos cualquier slash extra al final y agregamos la ruta /api estándar
         const baseUrlLimpia = process.env.TUMI_BASE_URL.replace(/\/+$/, '');
-        const response = await fetch(`${baseUrlLimpia}/api/payout`, {
+        const response = await fetch(`https://api-empresas.staging.topup.com.co/production/api/v1/payout`, {
             method: 'POST',
             headers: {
                 'Token-Top': process.env.TUMI_TOKEN_TOP, 
